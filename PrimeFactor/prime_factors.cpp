c6890e8b08b16b560ace97081c86735f8134f3bd
thelogic0 <thelogic@gmail.com>
2026-07-01 08:54:21 +0900

    [feature] success from 1 to 4

--- PrimeFactor/prime_factors.cpp
+++ PrimeFactor/prime_factors.cpp
@@ -5,6 +5,14 @@ using std::vector;
 class PrimeFactors {
 public:
 	vector<int> of(int i) {
-		return {};
+		vector<int> result = {};
+		if (i == 4) {
+			result.push_back(2);
+			result.push_back(2);
+		}
+		else if (i > 1) {
+			result.push_back(i);
+		}
+		return result;
 	}
 };
--- PrimeFactor/prime_factors_test.cpp
+++ PrimeFactor/prime_factors_test.cpp
@@ -1,9 +1,31 @@
 #include "gmock/gmock.h"
 #include "prime_factors.cpp"
 
-TEST(PrimeFactors, 0f1)
+TEST(PrimeFactors, Of1)
 {
 	PrimeFactors prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+
+TEST(PrimeFactors, Of2)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
+TEST(PrimeFactors, Of3)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 3 };
+	EXPECT_EQ(expected, prime_factor.of(3));
+}
+
+TEST(PrimeFactors, Of4)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 2, 2 };
+	EXPECT_EQ(expected, prime_factor.of(4));
 }
