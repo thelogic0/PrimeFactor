6907867a1c53d42c96f20e85edfe5aeec98d3dfd
thelogic0 <thelogic@gmail.com>
2026-07-01 08:47:03 +0900

    [feature] prime:1 basic test

---
+++ PrimeFactor/prime_factors_test.cpp
@@ -0,0 +1,9 @@
+#include "gmock/gmock.h"
+#include "prime_factors.cpp"
+
+TEST(PrimeFactors, 0f1)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
