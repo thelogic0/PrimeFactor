1fc675e539f9da7214027fdd4cae40b361d8ebfc
thelogic0 <thelogic@gmail.com>
2026-07-01 09:00:47 +0900

    [feature] 0..10

--- PrimeFactor/prime_factors.cpp
+++ PrimeFactor/prime_factors.cpp
@@ -4,14 +4,32 @@ using std::vector;
 
 class PrimeFactors {
 public:
-	vector<int> of(int i) {
+	vector<int> of(int number) {
 		vector<int> result = {};
-		if (i == 4) {
-			result.push_back(2);
-			result.push_back(2);
+		if (number == 4) {
+			while (number % 2 == 0) {
+				result.push_back(2);
+				number = number / 2;
+			}
 		}
-		else if (i > 1) {
-			result.push_back(i);
+		else if (number == 6) {
+			result.push_back(2);
+			result.push_back(3);
+		}
+		else if (number == 8) {
+			result.push_back(2);
+			result.push_back(4);
+		}
+		else if (number == 9) {
+			result.push_back(3);
+			result.push_back(3);
+		}
+		else if (number == 10) {
+			result.push_back(2);
+			result.push_back(5);
+		}
+		else if (number > 1) {
+			result.push_back(number);
 		}
 		return result;
 	}
--- PrimeFactor/prime_factors_test.cpp
+++ PrimeFactor/prime_factors_test.cpp
@@ -29,3 +29,45 @@ TEST(PrimeFactors, Of4)
 	vector<int> expected = { 2, 2 };
 	EXPECT_EQ(expected, prime_factor.of(4));
 }
+
+TEST(PrimeFactors, Of5)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 5 };
+	EXPECT_EQ(expected, prime_factor.of(5));
+}
+
+TEST(PrimeFactors, Of6)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
+
+TEST(PrimeFactors, Of7)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 7 };
+	EXPECT_EQ(expected, prime_factor.of(7));
+}
+
+TEST(PrimeFactors, Of8)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 2, 4 };
+	EXPECT_EQ(expected, prime_factor.of(8));
+}
+
+TEST(PrimeFactors, Of9)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 3, 3 };
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
+
+TEST(PrimeFactors, Of10)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = { 2, 5 };
+	EXPECT_EQ(expected, prime_factor.of(10));
+}
