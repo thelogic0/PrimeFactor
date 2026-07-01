ade66fa25263a9d299267d865329fd788bfc2d15
thelogic0 <thelogic@gmail.com>
2026-07-01 09:08:51 +0900

    [refactor] 0..10 success

--- PrimeFactor/prime_factors.cpp
+++ PrimeFactor/prime_factors.cpp
@@ -6,29 +6,15 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number == 4) {
-			while (number % 2 == 0) {
-				result.push_back(2);
-				number = number / 2;
+		for (int divider = 2; divider < number; ++divider) {
+			if (number % divider == 0) {
+				if (divider <= number / divider) {
+					result.push_back(divider);
+					result.push_back(number / divider);
+				}
 			}
 		}
-		else if (number == 6) {
-			result.push_back(2);
-			result.push_back(3);
-		}
-		else if (number == 8) {
-			result.push_back(2);
-			result.push_back(4);
-		}
-		else if (number == 9) {
-			result.push_back(3);
-			result.push_back(3);
-		}
-		else if (number == 10) {
-			result.push_back(2);
-			result.push_back(5);
-		}
-		else if (number > 1) {
+		if ((number != 1) && (result.size() == 0)) {
 			result.push_back(number);
 		}
 		return result;
